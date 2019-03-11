61c181dfbba8e4f5adac615af6ccf0ccc83b6550
terne <terne.thorn@gmail.com>
2019-03-11 23:08:27 +0100

    changes from local, working on liar dataset now

--- liar_logistic_regression.py
+++ liar_logistic_regression.py
@@ -21,14 +21,16 @@ tr = [s.lower() for s in tr if len(s) > 1] #NOTE: I cannot do this with the kagg
 #print(len(tr))
 trlab = codecs.open('data/liar_ytrain.txt', 'r', 'utf-8').read().split('\n')
 trlab = [s for s in trlab if len(s) > 1]
-te = codecs.open("data/liar_xtest.txt", 'r', 'utf-8').read().split('\n')
-te = [s.lower() for s in te if len(s) > 1]
-telab = codecs.open("data/liar_ytest.txt", 'r', 'utf-8').read().split('\n')
-telab = [s for s in telab if len(s) > 1]
 
+val = codecs.open("data/liar_xval.txt", 'r', 'utf-8').read().split('\n')
+val = [s.lower() for s in val if len(s) > 1]
+vallab = codecs.open("data/liar_yval.txt", 'r', 'utf-8').read().split('\n')
+vallab = [s for s in vallab if len(s) > 1]
+
+# check
 assert len(tr) == len(trlab)
 
-assert len(te) == len(telab)
+assert len(val) == len(vallab)
 
 random.seed(16)
 np.random.seed(16)
@@ -44,19 +46,22 @@ print('fitting X_tr')
 X_tr = vectorizer.fit_transform(tqdm.tqdm(tr)).toarray()
 print('done')
 print('fitting X_te')
-X_te = vectorizer.transform(tqdm.tqdm(te)).toarray()
+X_val = vectorizer.transform(tqdm.tqdm(val)).toarray()
 print('done')
 
 feats = ['_'.join(s.split()) for s in vectorizer.get_feature_names()] #de m ngrams modellen bruger
 
 print("fitting log reg")
 clf=None
-clf = LogisticRegression(random_state=16, solver='saga', penalty='l1', max_iter=1000, multi_class="multinomial").fit(X_tr, trlab)
+clf = LogisticRegression(random_state=16, solver='saga', penalty='l1', max_iter=10000, multi_class="multinomial").fit(X_tr, trlab)
 print("done")
 #print(clf.intercept_)
-y_hat = clf.predict(X_te)
-score=f1_score(te_lab, y_hat) #test accuracy er egentlig mindre vigtigt - det handler bare om at fitte. Det er dog meget smart så man kan se, at modellen lærer noget fornuftigt.
-print(score)
-allcoefs = pd.DataFrame.from_records(clf.coef_.tolist()[0], columns=feats) #add ngrams as colnames
 
-allcoefs.to_csv('allcoefs_'+str(m)+'-'+str(k)+'gram-l1_+'.csv', sep='\t', index=False)
+coefs = clf.coef_
+
+allcoefs = pd.DataFrame.from_records(coefs, columns=feats) #add ngrams as colnames
+
+allcoefs.to_csv('liar_coefs_'+str(m)+'-'+str(k)+'gram-l1'+'.csv', sep='\t', index=False)
+y_hat = clf.predict(X_val)
+score=f1_score(vallab, y_hat) #test accuracy er egentlig mindre vigtigt - det handler bare om at fitte. Det er dog meget smart så man kan se, at modellen lærer noget fornuftigt.
+print(score)
--- preprocessing_for_logreg.py
+++ preprocessing_for_logreg.py
@@ -1,12 +1,13 @@
+import numpy as np
+import pandas as pd
 
 
-
-liar_train = pd.read_csv("Liar/liar_dataset/train.tsv", sep="\t", header=None)
-liar_test = pd.read_csv("Liar/liar_dataset/test.tsv", sep="\t", header=None)
+liar_train = pd.read_csv("/Users/Terne/Documents/KU/Speciale/Liar/liar_dataset/train.tsv", sep="\t", header=None)
+liar_val = pd.read_csv("/Users/Terne/Documents/KU/Speciale/Liar/liar_dataset/valid.tsv", sep="\t", header=None)
 liar_xtrain = liar_train[2]
 liar_ytrain = liar_train[1]
-liar_xtest = liar_test[2]
-liar_ytest = liar_test[1]
+liar_xval = liar_val[2]
+liar_yval = liar_val[1]
 
 def make_txt_file_from_pandas_textcolumn(dataframe_column, savefile_name):
     with open(str(savefile_name), "w+") as file:
@@ -20,6 +21,6 @@ def save_labels_in_txt(dataframe_column, savefile_name):
             file.write(str(label))
             file.write("\n")
 
-make_txt_file_from_pandas_textcolumn(liar_train, "liar_xtrain.txt")
-
+#make_txt_file_from_pandas_textcolumn(liar_xval, "liar_xval.txt")
+save_labels_in_txt(liar_yval, "liar_yval.txt")
 #tr = codecs.open("liar_Xtrain", 'r', 'utf-8').read().split('\n')
