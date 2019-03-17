fd1562815cb2cd50a186c69433299c38fa36f59c
terne <terne.thorn@gmail.com>
2019-03-17 17:47:07 +0100

    updated sbatch script ready for cluster run

--- sbatchScript.sh
+++ sbatchScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=liarlogreg
+#SBATCH --job-name=lstmvisPrep
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=2-00:00:00
-python3 ./liar_logistic_regression.py 10000
+python3 ./lstmvis_prep.py
