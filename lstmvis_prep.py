1473ef293c388a52950118d0013f94fbc3ce4edb
terne <terne.thorn@gmail.com>
2019-03-22 21:23:24 +0100

    removed the batch size parameter

--- lstmvis_prep.py
+++ lstmvis_prep.py
@@ -104,7 +104,7 @@ lstm_out = LSTM(num_cells, return_sequences=True)(x)
 predictions = TimeDistributed(Dense(2, activation='softmax'))(lstm_out)
 model = Model(inputs=myInput, outputs=predictions)
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit({'input': seq}, y_train_tiled, epochs=num_epochs, batch_size=num_batch, verbose=1)
+model.fit({'input': seq}, y_train_tiled, epochs=num_epochs, verbose=1)
 
 model.layers.pop();
 model.summary()
@@ -112,7 +112,7 @@ model.summary()
 inp = model.input
 out = model.layers[-1].output
 model_RetreiveStates = Model(inp, out)
-states_model = model_RetreiveStates.predict(trainTextsSeq, batch_size=num_batch)
+states_model = model_RetreiveStates.predict(trainTextsSeq)
 print(states_model.shape)
 
 # Flatten first and second dimension for LSTMVis
