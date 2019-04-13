9cc9303ce0ab50e966d5517d16077a56c7499b53
terne <terne.thorn@gmail.com>
2019-04-13 13:25:36 +0200

    changed sbatchscript to run annotation script

--- sbatchScript.sh
+++ sbatchScript.sh
@@ -2,5 +2,5 @@
 #SBATCH --job-name=lstmvisPrep
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=2-00:00:00
-python3 ./lstmvis_prep.py
+#SBATCH --time=4-00:00:00
+python ./nlp_annotate.py
